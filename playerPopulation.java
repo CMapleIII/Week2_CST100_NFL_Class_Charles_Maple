c30f49d64b99e1c6ce393c95d4b9100c3f2236ec
Charles Maple III <peoriajuggalo1@gmail.com>
2016-07-14 16:30:27 -0500

    Add files via upload

---
+++ playerPopulation.java
@@ -0,0 +1,10 @@
+public class playerPopulation 
+{
+	public static void main(String[] args) 
+	{
+		PlayerManager pManager = new PlayerManager();
+		
+		System.out.println("Number of Players is " + pManager.listofPlayers.size());
+	}
+
+}
